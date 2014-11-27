ef155709c2975e72cefcbb4e425736cdbf99e230
Giuseppe <giuseppe.giordano1992@gmail.com>
2014-11-27 12:35:11 +0100

    fatto esempio jsf + spring + navigazione (navigation rules)

--- CorsoWebEsempi/EsempioSpringJsf/src/main/java/com/corsoweb/springjsf/FormDiEsempioBean.java
+++ CorsoWebEsempi/EsempioSpringJsf/src/main/java/com/corsoweb/springjsf/FormDiEsempioBean.java
@@ -1,18 +1,38 @@
 package com.corsoweb.springjsf;
 
+import java.io.Serializable;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("session")
-public class FormDiEsempioBean {
+public class FormDiEsempioBean implements Serializable {
 
 	private String nome;
 	private String cognome;
 	private int eta;
 
+	private String messaggioConferma;
+
 	public void registrati() {
 
+		messaggioConferma = "REGISTRAZIONE UTENTE: " + nome + " " + cognome
+				+ " CORRETTAMENTE AVVENUTA";
+	}
+
+	public String registratiConNavigazione() {
+
+		messaggioConferma = "REGISTRAZIONE UTENTE: " + nome + " " + cognome
+				+ " CORRETTAMENTE AVVENUTA";
+
+		return "registrazioneOk";
+	}
+
+	public void resetCampi() {
+		nome = "";
+		cognome = "";
+		eta = 0;
 	}
 
 	public void setNome(String nome) {
@@ -25,6 +45,26 @@ public class FormDiEsempioBean {
 
 	public void setEta(int eta) {
 		this.eta = eta;
+	}
+
+	public String getMessaggioConferma() {
+		return messaggioConferma;
+	}
+
+	public void setMessaggioConferma(String messaggioConferma) {
+		this.messaggioConferma = messaggioConferma;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public int getEta() {
+		return eta;
 	}
 
 }
